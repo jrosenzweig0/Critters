dabc8e38d10ef6e36743138af3b465b5ff5c5835
Unknown <zachsisti@gmail.com>
2018-03-22 18:25:33 -0500

    Debugging Critters 1 and 6

--- src/assignment4/MyCritter1.java
+++ src/assignment4/MyCritter1.java
@@ -11,7 +11,7 @@ public class MyCritter1 extends Critter.TestCritter{
 	 * @param friend Critter whose distance we are solving for
 	 * @return double representing distance to friend
 	 */
-	private double getDist(Critter friend){
+	public double getDist(Critter friend){
 		MyCritter1 fr = (MyCritter1)friend;							//cast to MyCritter1
 		double distance = sqrt((fr.getX_coord()-this.getX_coord())*(fr.getX_coord()-this.getX_coord())+	//distance formula
 				(fr.getY_coord()-this.getY_coord())*(fr.getY_coord()-this.getY_coord()));
@@ -23,31 +23,31 @@ public class MyCritter1 extends Critter.TestCritter{
 	 * @param friend Critter whose direction we are interested in
 	 * @return int from 0-8 representing direction that friend is in
 	 */
-	private int getDirec(Critter friend){
+	public int getDirec(Critter friend){
 		MyCritter1 fr = (MyCritter1)friend;							//cast to MyCritter1
 		double x = (double)(fr.getX_coord()-this.getX_coord());		//+ means to the right, - means to the left
-		double y = (double)(this.getY_coord()-fr.getY_coord());		//+ means up, - means down
+		double y = (double)(this.getY_coord()-this.getY_coord());		//+ means up, - means down
 		if (x==0 && y==0) return getRandomInt(8); //if in the same tile go in a random direcion
 		if (x==0) return (y>0) ? 2 : 6;				//if x is the same go up or down
 		if (y==0) return (x>0) ? 0 : 4;				//if y is the same go left or right
-		double angle=0.0;								//get angle
-        double frac=0.0;
-		if (x>0 && y>0) {						//first quad tan(y/x)
-		    frac = y/x;
-		    angle = Math.atan(frac);
-        }
+		double angle=0.0;				 				//get angle
+		double frac=0.0;
+		if (x>0 && y>0) {		 				//first quad tan(y/x)
+			frac = y/x;
+			angle = Math.atan(frac);
+		}
 		else if (x<0 && y>0) {					//second quad 180-tan(y/(-x))
-		    frac = y/((-1.0)*x);
-		    angle = (Math.PI)-Math.atan(frac);
-        }
+			frac = y/((-1.0)*x);
+			angle = (Math.PI)-Math.atan(frac);
+		}
 		else if (x<0 && y<0) {					//third quad 180+tan(y/x)
-		    frac = y/x;
-		    angle = (Math.PI)+Math.atan(frac);
-        }
+			frac = y/x;
+			angle = (Math.PI)+Math.atan(frac);
+		}
 		else {									//fourth quad 360-tan((-y)/x)
-		    frac = ((-1.0)*y)/x;
-		    angle = ((2.0)*Math.PI)-Math.atan(frac);
-        }
+			frac = ((-1.0)*y)/x;
+			angle = ((2.0)*Math.PI)-Math.atan(frac);
+		}
 		double direc = (angle/((2.0)*Math.PI))*(8.0);		//convert angle to int from 0 to 7
 		int dir = (int)Math.round(direc);
 		if (dir == 8) dir = 0;
@@ -59,15 +59,13 @@ public class MyCritter1 extends Critter.TestCritter{
 	 */
 	@Override
 	public void doTimeStep() {
-
-		if (this.getEnergy()>=15) {
-			try {                                                                //gets list of MyCritter1s
+		try {                                                                //gets list of MyCritter1s
 				List<Critter> friends = Critter.getInstances("assignment4.MyCritter1");
 				double bestDistance = getDist(friends.get(0));					//double representing distance to closest MyCritter1
 				int bestFriend = 0;												//index of closest MyCritter1 in friends
 				if ((friends.get(bestFriend) == this)&&(friends.size()>1)) {	//makes start Critter!=this
 					bestFriend = 1;
-					bestDistance = getDist(friends.get(bestFriend));
+					bestDistance = getDist( friends.get(bestFriend));
 				}
 				for (int i = 0; i < friends.size(); i++) {						//checks all MyCritter1s to find closest
 					if (getDist(friends.get(i)) < bestDistance && friends.get(i) != this) {
@@ -83,8 +81,7 @@ public class MyCritter1 extends Critter.TestCritter{
 			} catch (Exception e) {                                            //if other exception print stack trace
 				e.printStackTrace();
 			}
-		}
-		if (this.getEnergy()>=120) {											//if energy is above threshold (120) reproduce
+		if (this.getEnergy()>=100) {											//if energy is above threshold (120) reproduce
 			MyCritter1 baby = new MyCritter1();
 			reproduce(baby,getRandomInt(8));
 		}
@@ -99,6 +96,15 @@ public class MyCritter1 extends Critter.TestCritter{
 	public boolean fight(String opponent) {
 		if ((getEnergy() > 10) && (!opponent.equals("Z"))) return true;			//if energy is above threshold (10) and opponent is not a MyCritter1 fight
 		return false;
+	}
+
+	public static void runStats(java.util.List<Critter> My1s) {
+		int total=0;
+		for (int i=0; i<My1s.size(); i++){
+			total+=My1s.get(i).getEnergy();
+		}
+		System.out.print("" + My1s.size() + " total Critter1s    ");
+		System.out.println(" with an average of " + total/(My1s.size()) + "  energy");
 	}
 
 	/**
--- src/assignment4/MyCritter6.java
+++ src/assignment4/MyCritter6.java
@@ -2,20 +2,70 @@ package assignment4;
 
 import assignment4.Critter.TestCritter;
 
+import java.util.*;
+
+import static java.lang.Math.*;
+
 public class MyCritter6 extends TestCritter {
-	
+	/**
+	 * construct sets fought to 0
+	 */
+
+	public MyCritter6(){
+		fought=0;
+	}
+	private int fought;		//energy to be deducted at the beginning of the next time step (70% of energy at the time of the previous turn's fight)
+
+	/**
+	 * Determines if and how MyCritter6 will walk and reproduce (and lose energy if fought last turn) each timestep
+	 */
 	@Override
 	public void doTimeStep() {
+		if (fought>0) {						//if fought previous turn deduct fought
+			setEnergy(getEnergy()-fought);
+			fought=0;
+		}
+		if (getEnergy()<25){				//only move if energy is below threshold (25)
+			walk(getRandomInt(8));
+		}
+		if (getEnergy()>=60){				//only reproduce if energy is above threshold (120)
+			MyCritter6 child = new MyCritter6();
+			reproduce(child, getRandomInt(8));
+		}
 	}
 
+	/**
+	 * Determines if MyCritter6 will fight
+	 * @param opponent String representing opponent Class symbol
+	 * @return boolean indicating if MyCritter6 will fight
+	 */
 	@Override
 	public boolean fight(String opponent) {
-		run(getRandomInt(8));
+		if (getEnergy()>20){						//only fight if energy is above threshold (20)
+			fought = (int)(getEnergy()*(0.7));		//When entering a fight, increase energy by 50%, but lose 70% after fight ends
+			setEnergy(getEnergy()+(getEnergy()/2));
+			return true;
+		}
 		return false;
 	}
 
+	/**
+	 * gets symbol of MyCritter6
+	 * @return String representing symbol of MyCritter6
+	 */
 	@Override
 	public String toString () {
-		return "5";
+		return "!";
+	}
+
+	public static void runStats(java.util.List<Critter> My6s) {
+		int g100 = 0;
+		int l30 = 0;
+		for (int i=0; i<My6s.size(); i++){
+			if (My6s.get(i).getEnergy()>=100) g100++;
+			if (My6s.get(i).getEnergy()<30) l30++;
+		}
+		System.out.print("" + My6s.size() + " total Critter6s    ");
+		System.out.println("" + g100 + " thriving, and " + l30 + " on death's door");
 	}
 }
